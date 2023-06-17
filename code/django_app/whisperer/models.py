c6340f98b9fe423f8c63a3f9783d7aa3ca64d27a
ahsin-s <ashabbi00@gmail.com>
2023-06-17 01:32:54 -0400

    work in progress on the complete deployment

--- code/django_app/whisperer/models.py
+++ code/django_app/whisperer/models.py
@@ -1,3 +1,20 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+
+# Model for storing the whisper
+class Whisper(models.Model):
+    whisper = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+
+    def __str__(self):
+        return self.whisper
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
--- code/django_app/whisperer/views.py
+++ code/django_app/whisperer/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from models import Whisper
+
+
+# view to retrieve the whispers in sorted order by pub date
+def index(request):
+    # query all records in Whisper model
+    whispers = Whisper.objects.all().order_by('-pub_date')
+    # render the index.html template with the whispers
+    return render(request, 'whisper/index.html', {
+        'whispers': whispers
+    })
+
+
+def detail(request, whisper_id):
+    # query the whisper with the given id
+    whisper = Whisper.objects.get(id=whisper_id)
+    # render the detail.html template with the whisper
+    return render(request, 'whisper/detail.html', {
+        'whisper': whisper
+    })
+
+
+# view for adding new entries to Whisper model
+def add(request):
+    # create a new whisper
+    whisper = Whisper(
+        whisper=request.POST['content'],
+    )
+    # save the new whisper
+    whisper.save()
+    # redirect to the index page
+    return redirect('/whisper/')
